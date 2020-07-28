ed6c820738fec0893d5fef22053f554d88325b4e
trungduong0103 <trungduong0103@gmail.com>
2020-07-28 13:57:19 +0700

    add "origin" tag to QR Code content

--- functions/handlers/qr-code/index.js
+++ functions/handlers/qr-code/index.js
@@ -1,5 +1,7 @@
 const QRCode = require('qrcode');
 exports.generateQrCode = (req, res) => {
+  const QRCodeContent = req.body.content;
+  QRCodeContent.origin = 'FRAA-CheckIn';
   return QRCode.toDataURL(JSON.stringify(req.body.content), (error, url) => {
     if (error) {
       res.send(error);
