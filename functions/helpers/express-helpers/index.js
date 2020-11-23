ea778cfb33c7b28a37cee4ae05c0bc74d9b38ad6
trungduong0103 <trungduong0103@gmail.com>
2020-11-23 17:07:08 +0700

    return 200 for bad request

--- functions/helpers/express-helpers/index.js
+++ functions/helpers/express-helpers/index.js
@@ -1,9 +1,9 @@
 exports.sendErrorMessage = (response, message) => {
-  return response.status(400).send({ error: message });
+  return response.status(200).send({ error: message });
 };
 
 exports.sendErrorObject = (response, error) => {
-  return response.status(400).send({ error });
+  return response.status(200).send({ error });
 };
 
 exports.sendSuccessMessage = (response, message) => {
