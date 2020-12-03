548a382b677e4df5c432d0f9beea7a68528ee691
pham-hang <phamngocminhhang@gmail.com>
2020-12-03 16:16:35 +0700

    Read the collection in firebase

--- publisher/main.py
+++ publisher/main.py
@@ -2,15 +2,15 @@ import firebase_admin
 import google.cloud
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("./ServiceAccountKey.json")
+cred = credentials.Certificate("./settings/fraa-capstone.json")
 app = firebase_admin.initialize_app(cred)
 
 store = firestore.client()
-doc_ref = store.collection(u'users').limit(2)
+doc_ref = store.collection('students')
 
 try:
     docs = doc_ref.get()
     for doc in docs:
-        print(u'Doc Data:{}'.format(doc.to_dict()))
+        print(doc.to_dict())
 except google.cloud.exceptions.NotFound:
-    print(u'Missing data')
+    print('Missing data')
